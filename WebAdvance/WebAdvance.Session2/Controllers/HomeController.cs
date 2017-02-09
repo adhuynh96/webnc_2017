934a7e1308580c07f01dc402c848edf9587a9b1d
phantrithuc <phantrithuc@gmail.com>
2017-02-09 09:04:09 +0700

    Session 2_2017_02_09

--- WebAdvance/WebAdvance.Session2/Controllers/HomeController.cs
+++ WebAdvance/WebAdvance.Session2/Controllers/HomeController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAdvance.Session2.Models;
 
 namespace WebAdvance.Session2.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello World";
+            ViewBag.Student = "Thuc";      
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RSVPForm()
+        {            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RSVPForm(GuestResponse response)
+        {
+            return View("Thanks", response);
         }
     }
 }
